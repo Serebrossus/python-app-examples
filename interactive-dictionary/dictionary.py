2ac85cefb288b71e2a91687a9d11f63500fa4a1b
Serebrossus <pidtchay@gmail.com>
2019-02-02 17:11:25 +0300

    check for non existing word #1st elif : to make sure the program return the definition of words that start with a capital letter #2nd elif : to make sure the program return the definition of acronyms

--- interactive-dictionary/dictionary.py
+++ interactive-dictionary/dictionary.py
@@ -5,7 +5,19 @@ data = json.load(open('dictionary.json'))
 
 #function for retriving definition
 def retrive_definition(word):
-    return data[word]
+    #converting all lettersto lower because out data is in that format
+    word = word.lower()
+    #check for non existing word
+    #1st elif : to make sure the program return the definition of words that start with a capital letter
+    #2nd elif : to make sure the program return the definition of acronyms
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    else:
+        return 'the word does not exit, please double check it.'
 
 #input for user
 word_user = input('Enter a word: ')
