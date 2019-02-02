d2dd42dee5d591d3dbc770e19e8c5af4b42f2f1e
Serebrossus <pidtchay@gmail.com>
2019-02-02 16:11:16 +0300

    first commit. create python app interactive dictionary

---
+++ interactive-dictionary/dictionary.py
@@ -0,0 +1,15 @@
+import json
+
+#loading json data.
+data = json.load('dictionary.json')
+
+#function for retriving definition
+def retrive_definition(word):
+    return data[word]
+
+#input for user
+word_user = input('Enter a word: ')
+
+
+#retriving definition using function and print result
+print(retrive_definition(word_user))
