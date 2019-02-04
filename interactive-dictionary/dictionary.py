bff2098ba7eedaea8c6cce3b4fd1c91271447c81
Serebrossus <tkbaltazar@gmail.com>
2019-02-04 13:57:33 +0300

    add action Did you mean instead [y or n]

--- interactive-dictionary/dictionary.py
+++ interactive-dictionary/dictionary.py
@@ -19,7 +19,14 @@ def retrive_definition(word):
     elif word.upper() in data:
         return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return ('Did you mean %s instead?' % get_close_matches(word, data.keys())[0])
+        action = input('Did you mean %s instead? [y or n]' % get_close_matches(word, data.keys())[0])
+        #if answer is yes, retrive deffinition of suggested word
+        if action == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif action == "n":
+            return ("The word doesn't exist")
+        else:
+            return ("We don't understand your entry")
 
 #input for user
 word_user = input('Enter a word: ')
