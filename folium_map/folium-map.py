2e281c9b69d466a2d98b381b0d39778886dcdebd
Serebrossus <pidtchay@gmail.com>
2019-02-06 09:21:58 +0300

    change icons

--- folium_map/folium-map.py
+++ folium_map/folium-map.py
@@ -11,7 +11,7 @@ elevation = data['ELEV']
 def color_change(elevation):
     if(elevation < 1000):
         return ('green')
-    elif(1000 <= elevation):
+    elif(1000 <= elevation < 3000):
         return ('orange')
     else:
         return ('red')
@@ -21,7 +21,8 @@ map = folium.Map(location=[37.296933-121.9574983], zoom_start=5, title='Mapbox b
 
 #Plot markers
 for lat, lon, elevation in zip(lat, lon, elevation):
-    folium.Marker(location=[lat, lon], popup=str(elevation) + ' m', icon=folium.Icon(color=color_change(elevation))).add_to(map)
+    folium.Marker(location=[lat, lon], radius=9, popup=str(elevation) + ' m', fill_color=color_change(elevation),
+                  icon=folium.Icon(color='grey', fill_opacity=0.9)).add_to(map)
 
 #save map
 map.save("map1.html")
