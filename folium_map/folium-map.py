8f671adb16685e6fbf21add4fa77faeee0986c52
Serebrossus <pidtchay@gmail.com>
2019-02-06 09:06:16 +0300

    add multiple markers to map

--- folium_map/folium-map.py
+++ folium_map/folium-map.py
@@ -3,8 +3,9 @@ import folium
 #create base map
 map = folium.Map(location=[37.296933-121.9574983], zoom_start=8, title='Mapbox bright')
 
-#Add marker
-folium.Marker(location=[37.4074687-122.086669], popup='Google HQ', icon=folium.Icon(color='gray')).add_to(map)
+#Multiple markers
+for coordinates in [[37.4074687-122.086669], [37.8199286-122.4804438]]:
+    folium.Marker(location=coordinates, icon=folium.Icon(color='gray')).add_to(map)
 
 #save map
 map.save("map1.html")
