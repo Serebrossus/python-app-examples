fb6d1e86ea825a578a533f8bc4b69b0ebb83b475
Serebrossus <pidtchay@gmail.com>
2019-02-06 09:19:19 +0300

    function to change color

--- folium_map/folium-map.py
+++ folium_map/folium-map.py
@@ -7,12 +7,21 @@ lat = data['LAT']
 lon = data['LON']
 elevation = data['ELEV']
 
+#function to change color
+def color_change(elevation):
+    if(elevation < 1000):
+        return ('green')
+    elif(1000 <= elevation):
+        return ('orange')
+    else:
+        return ('red')
+
 #create base map
 map = folium.Map(location=[37.296933-121.9574983], zoom_start=5, title='Mapbox bright')
 
 #Plot markers
 for lat, lon, elevation in zip(lat, lon, elevation):
-    folium.Marker(location=[lat, lon], popup=str(elevation) + ' m', icon=folium.Icon(color='gray')).add_to(map)
+    folium.Marker(location=[lat, lon], popup=str(elevation) + ' m', icon=folium.Icon(color=color_change(elevation))).add_to(map)
 
 #save map
 map.save("map1.html")
