95680b6498b69d4eb1a24d7bb83ee6ca2bb3c6d9
Serebrossus <pidtchay@gmail.com>
2019-02-06 09:16:46 +0300

    load data. plot markers

--- folium_map/folium-map.py
+++ folium_map/folium-map.py
@@ -1,11 +1,18 @@
 import folium
+import pandas as pd
+
+#Load data
+data = pd.read_csv("Vulcanoes_USA.txt")
+lat = data['LAT']
+lon = data['LON']
+elevation = data['ELEV']
 
 #create base map
-map = folium.Map(location=[37.296933-121.9574983], zoom_start=8, title='Mapbox bright')
+map = folium.Map(location=[37.296933-121.9574983], zoom_start=5, title='Mapbox bright')
 
-#Multiple markers
-for coordinates in [[37.4074687-122.086669], [37.8199286-122.4804438]]:
-    folium.Marker(location=coordinates, icon=folium.Icon(color='gray')).add_to(map)
+#Plot markers
+for lat, lon, elevation in zip(lat, lon, elevation):
+    folium.Marker(location=[lat, lon], popup=str(elevation) + ' m', icon=folium.Icon(color='gray')).add_to(map)
 
 #save map
 map.save("map1.html")
