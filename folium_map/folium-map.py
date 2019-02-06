5c0672b44d585b2a81859363fec37d682dc04661
Serebrossus <pidtchay@gmail.com>
2019-02-06 09:24:58 +0300

    create markers cluster

--- folium_map/folium-map.py
+++ folium_map/folium-map.py
@@ -1,4 +1,5 @@
 import folium
+from folium.plugins import MarkerCluster
 import pandas as pd
 
 #Load data
@@ -19,10 +20,12 @@ def color_change(elevation):
 #create base map
 map = folium.Map(location=[37.296933-121.9574983], zoom_start=5, title='Mapbox bright')
 
+#create cluster
+marker_cluster = MarkerCluster().add_to(map)
 #Plot markers
 for lat, lon, elevation in zip(lat, lon, elevation):
-    folium.Marker(location=[lat, lon], radius=9, popup=str(elevation) + ' m', fill_color=color_change(elevation),
-                  icon=folium.Icon(color='grey', fill_opacity=0.9)).add_to(map)
+    folium.CircleMarker(location=[lat, lon], radius=9, popup=str(elevation) + ' m', fill_color=color_change(elevation),
+                  icon=folium.Icon(color='grey', fill_opacity=0.9)).add_to(marker_cluster)
 
 #save map
 map.save("map1.html")
