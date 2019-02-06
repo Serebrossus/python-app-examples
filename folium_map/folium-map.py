dbf0280f921dde8a34852db4bbad87db212eca9c
Serebrossus <pidtchay@gmail.com>
2019-02-06 09:02:28 +0300

    add marker to map

--- folium_map/folium-map.py
+++ folium_map/folium-map.py
@@ -1,7 +1,10 @@
 import folium
 
 #create base map
-map = folium.Map(location=[37.296933-121.9574983], zoom_start=8)
+map = folium.Map(location=[37.296933-121.9574983], zoom_start=8, title='Mapbox bright')
+
+#Add marker
+folium.Marker(location=[37.4074687-122.086669], popup='Google HQ', icon=folium.Icon(color='gray')).add_to(map)
 
 #save map
 map.save("map1.html")
