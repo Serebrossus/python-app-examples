9fe678eac89c1ee1065f6c14f19573fa4b8d0b41
Serebrossus <pidtchay@gmail.com>
2019-02-08 14:10:58 +0300

    scraping

--- pseudo-scraper/pseudo-scraper.py
+++ pseudo-scraper/pseudo-scraper.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 url_path = input('Enter site url path with http(s): ')
+# For example, I use the information from the site
+# "https://www.fifa.com/worldcup/players/player/201200/profile.html"
+# This program is written for informational purposes.
+# The task is to learn how to use the capabilities of python for writing different applications.
+
 if len(url_path) == 0:
     print('url path is empty')
 else:
-    #download page
+    # download page
     page = requests.get(url_path)
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -13,4 +19,3 @@ else:
     country = soup.find("div", {"class": "fi-p__country"}).text.replace("\n", "").strip()
     role = soup.find("div", {"class": "fi-p__role"}).text.replace("\n", "").strip()
     print(name, "\n", country, "\n", role, "\n")
-
