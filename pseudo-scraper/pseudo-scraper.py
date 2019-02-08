6b5126dd3fff281536acdddf912c80d43c99a6e0
Serebrossus <pidtchay@gmail.com>
2019-02-08 14:02:07 +0300

    scraping

--- pseudo-scraper/pseudo-scraper.py
+++ pseudo-scraper/pseudo-scraper.py
@@ -8,5 +8,9 @@ else:
     #download page
     page = requests.get(url_path)
     soup = BeautifulSoup(page.content, "html.parser")
-    print(soup.prettify())
+
+    name = soup.find("div", {"class": "fi-p__name"}).text.replace("\n", "").strip()
+    country = soup.find("div", {"class": "fi-p__country"}).text.replace("\n", "").strip()
+    role = soup.find("div", {"class": "fi-p__role"}).text.replace("\n", "").strip()
+    print(name, "\n", country, "\n", role, "\n")
 
