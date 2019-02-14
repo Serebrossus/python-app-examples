898052266a4e1cb93aeebc38cf3ff45dbaeffc1c
Serebrossus <pidtchay@gmail.com>
2019-02-14 17:28:54 +0300

    add check login and password verification

--- sockets/server/server2.py
+++ sockets/server/server2.py
@@ -1,15 +1,44 @@
+import socket
 import socketserver
 import sys
-# from hashlib import md5
+from hashlib import md5
 import threading
 import time
+
+
+
+password_hash = '5f4dcc3b5aa765d61d8327deb882cf99'
 
 
 # Async Mixin from SocketServer documentation
 class RequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
+        self.request.sendall("Auth panel\nEnter username: ".encode('utf-8'))
+        username = self.request.recv(1024).strip()
+
+        print("username: " + username.decode('utf-8'))
+
+        if username.decode('utf-8') != 'admin':
+            self.request.sendall("[-] Username is invalid\nExit".encode('utf-8'))
+            return -1
+
+        self.request.sendall("Enter password: ".encode('utf-8'))
+
+        password = self.request.recv(1024).strip()
+
+        print("password: " + password.decode('utf-8'))
+
+        hashed_password = md5(password).hexdigest()
+
+        if hashed_password != password_hash:
+            self.request.sendall("[-] Password is invalid\nExit".encode('utf-8'))
+            return -2
+
+        self.request.sendall("Welcome Admin\n".encode('utf-8'))
+
         data = self.request.recv(1024)
+
         cur_thread = threading.current_thread()
         response = "{}: {}".format(cur_thread.name, data)
         self.request.sendall(response.encode('utf-8'))
