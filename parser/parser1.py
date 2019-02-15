b13f875e2f2b6459a4380b46f9ea433b5e21e1ee
Serebrossus <pidtchay@gmail.com>
2019-02-15 12:48:14 +0300

    An example of parsing a file. The data in the file corresponds to the User class.

---
+++ parser/parser1.py
@@ -0,0 +1,36 @@
+import sys
+
+
+class User:
+    def __init__(self, buf):
+        buf = buf.split(' ')
+        self.name = buf[0].strip()
+        self.surename = buf[1].strip()
+        self.age = buf[2].strip()
+
+    def show(self):
+        print("Name %s" % self.name)
+        print("SureName %s" % self.surename)
+        print("Age %s" % self.age)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        print("Usage python " + sys.argv[0] + " <filename>")
+        sys.exit(-1)
+
+    file = open(filename, 'r')
+    buff = file.read()
+    file.close()
+
+    buff = buff.split("\n")
+    users = []
+    for i in buff:
+        users.append( User(i))
+
+    for i in users:
+        i.show()
+
+    print(buff)
