4f78af7880ee428832f344f30adf25341b0d1ebb
Serebrossus <pidtchay@gmail.com>
2019-02-22 15:23:06 +0300

    python 3 capture image. Use pygame and opencv

---
+++ capture-image/capture-image-pygame.py
@@ -0,0 +1,8 @@
+import pygame.camera
+
+pygame.camera.init()
+# camera detected or no
+cam = pygame.camera.Camera(pygame.camera.list_cameras()[0], (640, 480))
+cam.start()
+img = cam.get_image()
+pygame.image.save(img, 'pygame-screen.jpg')
