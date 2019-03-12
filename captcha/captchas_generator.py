732cc5ecfebe3bc33d692af439496f767da7a33b
Serebrossus <pidtchay@gmail.com>
2019-03-12 09:52:55 +0300

    update captcha generator

--- captcha/captchas_generator.py
+++ captcha/captchas_generator.py
@@ -1,22 +1,57 @@
+import os
 import random
+
+import numpy as np
+from PIL import Image
+from captcha.image import ImageCaptcha
 
 characters = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789'
 CAPTCHA_LENGTH = 4
+CAPTCHA_WIDTH = 240
+CAPTCHA_HEIGTH = 80
+CAPTCHA_DIR = 'images'
 
 
 def random_string():
+    """
+    generate random text from captcha
+    :return:
+    """
     rnd_string = (random.choice(characters) for _ in range(CAPTCHA_LENGTH))
     return ''.join(rnd_string)
 
 
 def generate_captcha(count):
+    """
+    :param count: how many times generate captcha
+    :return:
+    """
     for _ in range(int(count)):
-        print(random_string())
+        captcha_text = random_string()
+        generate_image_captcha(captcha_text)
+        print(captcha_text)
 
 
-generate_captcha(10000)
+def generate_image_captcha(captcha_text, width=CAPTCHA_WIDTH, height=CAPTCHA_HEIGTH):
+    """
+    :param captcha_text: input text
+    :param width: image width
+    :param height: image height
+    :return: captcha text and array from captcha image
+    """
 
-# TODO
-# 1. generate captcha image by text
-# 2. save image captcha to file with name captha text
-# 3. profit
+    if not os.path.exists(CAPTCHA_DIR):
+        os.makedirs(CAPTCHA_DIR)
+    image = ImageCaptcha(width=width, height=height)
+    captcha = image.generate(captcha_text)
+    image.write(captcha_text, os.path.join(CAPTCHA_DIR, captcha_text + '.png'))
+    captcha_image = Image.open(captcha)
+    captcha_image = np.array(captcha_image)
+    return captcha_text, captcha_image
+
+
+generate_captcha(10)
+
+# generates captcha text and saves it to file
+# text, c_image = generate_image_captcha(random_string())
+# print('Text: {} image: {}'.format(text, c_image))
